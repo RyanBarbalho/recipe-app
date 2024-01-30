8a217686c593257bae29128999b7be29c9dcc476
RyanBarbalho <rgtbc@ic.ufal.br>
2024-01-30 14:05:14 -0300

    testando tests e database service adicionado

--- app/app/calc.py
+++ app/app/calc.py
@@ -1,11 +1,13 @@
 """
-Calculator funcions
+Calculator functions
 """
 
-def add(x,y):
-    """Add x and y return result"""
-    return x+y
 
-def subtract(x,y):
+def add(x, y):
+    """Add x and y return result"""
+    return x + y
+
+
+def subtract(x, y):
     """Subtract x from y and return result"""
-    return x-y
+    return x - y
--- app/app/tests.py
+++ app/app/tests.py
@@ -12,9 +12,8 @@ class CalcTests(SimpleTestCase):
         """Test adding numbers together"""
         res = calc.add(5, 6)
         self.assertEqual(res, 11)
-        
+
     def test_subtract(self):
         """Test subtracting numbers together"""
         res = calc.subtract(5, 6)
         self.assertEqual(res, -1)
-        
