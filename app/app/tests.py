23de423df6a692b893faf2eb692530195717e0c5
RyanBarbalho <rgtbc@ic.ufal.br>
2024-01-30 11:51:23 -0300

    testestestando tests e database service adicionado

---
+++ app/app/tests.py
@@ -0,0 +1,19 @@
+"""
+Sample tests
+"""
+from django.test import SimpleTestCase
+
+from app import calc
+
+
+class CalcTests(SimpleTestCase):
+
+    def test_add(self):
+        """Test adding numbers together"""
+        res = calc.add(5,6)
+        self.assertEqual(res, 11)
+        
+    def test_subtract(self):
+        """Test subtracting numbers together"""
+        res = calc.subtract(5,6)
+        self.assertEqual(res, -1)
